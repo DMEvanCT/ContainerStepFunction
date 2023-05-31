1bf60e367618f5db64b9124a0fbabf061ee6e2a6
Evan Haston <ehaston@darkmatterit.io>
2023-05-30 22:25:15 -0400

    Initial commit

---
+++ Container/go.mod
@@ -0,0 +1,7 @@
+module github.com/DMEvanCT/v1/ContainerStepFunction
+
+go 1.20
+
+require github.com/aws/aws-sdk-go v1.44.272
+
+require github.com/jmespath/go-jmespath v0.4.0 // indirect
---
+++ Container/main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/sfn"
+	"log"
+	"os"
+)
+
+type EmployeeInfo struct {
+	EmployeeId int    `json:"employeeId"`
+	FirstName  string `json:"firstName"`
+	LastName   string `json:"lastName"`
+	City       string `json:"city"`
+	State      string `json:"state"`
+}
+
+type Employee struct {
+	EmployeeInfo EmployeeInfo `json:"employeeInfo"`
+}
+
+type TaskFailure struct {
+	Error string `json:"error"`
+	Cause string `json:"cause"`
+}
+
+func main() {
+	TaskToken := os.Getenv("TASK_TOKEN")
+	employeeData := os.Getenv("EMPLOYEE_JSON_ENV")
+	region := os.Getenv("AWS_REGION")
+
+	sess, err := session.NewSession(&aws.Config{
+		Region: aws.String(region),
+	})
+	if err != nil {
+		fmt.Println("Error creating session: ", err)
+
+	}
+	stepFunction := sfn.New(sess)
+
+	var emp Employee
+
+	jsonError := json.Unmarshal([]byte(employeeData), &emp)
+	if jsonError != nil {
+		sfn.SendTaskFailure(&sfn.SendTaskFailureInput{
+			Cause:     aws.String("JSON Parse Error"),
+			Error:     aws.String("Invalid JSON"),
+			TaskToken: aws.String(TaskToken),
+		})
+	}
+
+	fmt.Printf("Parsed data: %+v\n", emp)
+	_, err = stepFunction.SendTaskSuccess(&sfn.SendTaskSuccessInput{
+		Output:    aws.String(employeeData),
+		TaskToken: aws.String(TaskToken),
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+}
